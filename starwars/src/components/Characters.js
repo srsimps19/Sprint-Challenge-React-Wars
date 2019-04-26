9430994640cf918d6e4dba7fb7aa3df8f9fe731e
Sydney Crumley <srsimps19@gmail.com>
2019-04-26 14:53:54 -0500

    changed props name to correct name

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 function Characters(props) {
     let classNames = "character"
+    console.log(props)
     return (
         <div className={classNames}>
-            <h3>{props.character.name}</h3>
+            <h3>{props.char.name}</h3>
             <p>
-                <strong>Birth Year: </strong> {props.character.birthYear}
+                <strong>Birth Year: </strong> {props.char.birth_year}
             </p>
         </div>
     );
